8c8f6ac7e1a306615387a4fa51e15f88db3b3a6d
Brendan Ashby <ashbyb@rpi.edu>
2011-03-05 23:17:47 -0500

    - initial push of working NetBeans + JRuby + Ruby on Rails + WBrick 1.3.1 files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RCOSPLANer2::Application.config.secret_token = '82db821b00739652d9fd58b2c217f7d6ac69f733afd7f17094f75058ad7000518e6b6ac9137c6278e4ca44522707691b6b0abdf76c78f37c4f3462213e69b856'
